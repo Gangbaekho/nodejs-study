78b1953eb9b6c3ba39df28b0bab484e492b8af9e
kimjinsoo <roorooro9933@gmail.com>
2021-02-01 13:09:14 +0900

    understanding express.use() and using body parser middleware

--- app.js
+++ app.js
@@ -1,31 +1,38 @@
 const http = require("http");
 const express = require("express");
+const bodyParser = require("body-parser");
 
-// Express는 그냥 Middleware다?
-// 그래서 express()를 한 것도 결국 middleware function을 불러오는
-// 것에 불과하는 걸지도 모른다.
 const app = express();
 
-// 결국에 app.use()를 쓴다는 것 자체가
-// 어떤 middleware를 추가시키겠다는 의미로 받아들이면 된다.
-// next()라는 것은 그 다음 middleware로 가라는 의미니까
-// 여기서는 순서가 중요하게 된다.
-app.use((req, res, next) => {
-  console.log("In the middleware");
+// body 내용을 Parsing 하기 위한 Middleware를 추가시켜주었다
+// extended는 뭔지 모르니까 나중에 참고해보자.
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// use(path, requestHandler) 순으로 넣어주면된다.
+// 근데 use보다는 정확하게 get, post, delete, patch, put 이런
+// http method를 쓰는게 좋음, exact가 걸려있기 때문이다.
+// use를 쓰면 match되는건 다 걸리기 때문에 귀찮음.
+// 이런식으로 쓰면은 모든 url은 /을 포함하고 있기 때문에
+// 모든 요청에서 바로 아래의 middleware 로직을 처리하게 될 것임.
+app.use("/", (req, res, next) => {
+  console.log("This always runs!");
   next();
 });
 
-app.use((req, res, next) => {
-  console.log("In another middleware");
-  // 이렇게 send를 해주기만 하면 response를 전달해준다.
-  // 그리고 Content-Type도 알아서 text/html로 해준다.
-  // default 값이 그거인가보다.
-  res.send("<h1>Hello from Express!</h1>");
+app.use("/add-product", (req, res, next) => {
+  res.send(
+    '<form action="product" method="POST"><input type="text" name="title"/><button type="submit>SEND</button></form>'
+  );
+});
+
+app.use("/product", (req, res, next) => {
+  // 이렇게 하면 원랴 req의 body의 내용을 볼 수 있어야 하는데,
+  // 미들웨어 추가 안해주면은 이 처리를 자동으로 해주지 않음.
+  // 원래 chunk 뭐시기 해서 엄청 귀찮기 떄문이다.
+  console.log(req.body);
+  // 그냥 redirect 하는 방법과, redirect 할떄의 status 값을
+  // 배워가면 된다.
+  res.status(304).redirect("/");
 });
 
 app.listen(3000);
-
-// 밑에꺼 이제 필요없다.
-// const server = http.createServer(app);
-
-// server.listen(3000);
