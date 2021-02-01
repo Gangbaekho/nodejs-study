466aab627c8e3ca629ffbff93099c897a20b884d
kimjinsoo <roorooro9933@gmail.com>
2021-02-01 13:42:57 +0900

    understanding express.static() method

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const http = require("http");
 const express = require("express");
 const bodyParser = require("body-parser");
 const fs = require("fs");
+const path = require("path");
+const rootDir = require("./util/path");
 
 // ROUTERS
 const adminRoutes = require("./routes/admin");
@@ -10,13 +12,14 @@ const shopRoutes = require("./routes/shop");
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(express.static(path.join(__dirname, "public")));
+
 app.use("/admin", adminRoutes);
 app.use("/shop", shopRoutes);
 
-// 이런식으로 앞에 url을 안붙이면은
-// match 안된건 다 여기서 처리하겠다는 걸로 보면 되겠다.
 app.use((req, res, next) => {
-  res.status(404).send("<h1>Page not found. 404 error</h1>");
+  res.sendFile(path.join(rootDir, "views", "404.html"));
 });
 
 app.listen(3000);
