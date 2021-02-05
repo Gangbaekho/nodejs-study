fd639b77476580647b2233089b1bdf3bde4db97b
roorooro1 <roorooro9933@gmail.com>
2021-02-05 17:28:20 +0900

    using findAll method

--- controllers/shop.js
+++ controllers/shop.js
@@ -32,17 +32,28 @@ exports.getProduct = (req, res, next) => {
 };
 
 exports.getIndex = (req, res, next) => {
-  Product.fetchAll()
-    .then(([rows, fieldData]) => {
+  Product.findAll()
+    .then(products => {
       res.render("shop/index", {
-        prods: rows,
+        prods: products,
         pageTitle: "Shop",
         path: "/",
       });
     })
-    .catch((err) => {
-      console.log(err);
-    });
+    .catch(error => {
+      console.log(error)
+    })
+  // Product.fetchAll()
+  //   .then(([rows, fieldData]) => {
+  //     res.render("shop/index", {
+  //       prods: rows,
+  //       pageTitle: "Shop",
+  //       path: "/",
+  //     });
+  //   })
+  //   .catch((err) => {
+  //     console.log(err);
+  //   });
 };
 
 exports.getCart = (req, res, next) => {
