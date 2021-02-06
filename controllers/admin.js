522441dfc08ca9cd1f2db9f6a5d848e103d7c5d8
roorooro1 <roorooro9933@gmail.com>
2021-02-06 21:52:43 +0900

    using some methods to crud new versions!

--- controllers/admin.js
+++ controllers/admin.js
@@ -13,29 +13,20 @@ exports.postAddProduct = (req, res, next) => {
   const imageUrl = req.body.imageUrl;
   const price = req.body.price;
   const description = req.body.description;
-  Product.create({
-    title: title,
-    price: price,
-    imageUrl: imageUrl,
-    description: description
-  })
+  req.user
+    .createProduct({
+      title: title,
+      price: price,
+      imageUrl: imageUrl,
+      description: description
+    })
     .then(result => {
-      // console.log(result)
       console.log('Created Product')
       res.redirect("/admin/products");
     })
     .catch(error => {
       console.log(error)
     })
-  // const product = new Product(null, title, imageUrl, description, price);
-  // product
-  //   .save()
-  //   .then(() => {
-  //     res.redirect("/");
-  //   })
-  //   .catch((err) => {
-  //     console.log(err);
-  //   });
 };
 
 exports.getEditProduct = (req, res, next) => {
@@ -44,8 +35,10 @@ exports.getEditProduct = (req, res, next) => {
     return res.redirect("/");
   }
   const prodId = req.params.productId;
-  Product.findByPk(prodId)
-    .then(product => {
+  req.user.getProducts({ where: { id: prodId } })
+    // Product.findByPk(prodId)
+    .then(products => {
+      const product = products[0]
       if (!product) {
         return res.redirect("/");
       }
@@ -85,7 +78,7 @@ exports.postEditProduct = (req, res, next) => {
 };
 
 exports.getProducts = (req, res, next) => {
-  Product.findAll()
+  req.user.getProducts()
     .then(products => {
       res.render("admin/products", {
         prods: products,
