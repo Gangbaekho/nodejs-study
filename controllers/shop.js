51dac3cf396cb3c90df897e014f2f81e77b524e0
kimjinsoo <roorooro9933@gmail.com>
2021-02-08 14:15:05 +0900

    fetching and ouputting orders

--- controllers/shop.js
+++ controllers/shop.js
@@ -157,10 +157,18 @@ exports.postCartDeleteProduct = (req, res, next) => {
 };
 
 exports.getOrders = (req, res, next) => {
-  res.render("shop/orders", {
-    path: "/orders",
-    pageTitle: "Your Orders",
-  });
+  req.user
+    .getOrders({ include: ["products"] })
+    .then((orders) => {
+      res.render("shop/orders", {
+        path: "/orders",
+        pageTitle: "Your Orders",
+        orders: orders,
+      });
+    })
+    .catch((error) => {
+      console.log(error);
+    });
 };
 
 exports.getCheckout = (req, res, next) => {
