c4e84e9cd98ae3ebcc6dd60bb3caaf5723f0bc02
kimjinsoo <roorooro9933@gmail.com>
2021-02-10 15:24:32 +0900

    relation cart and user with nested document!

--- models/user.js
+++ models/user.js
@@ -3,14 +3,26 @@ const ObjectId = mongodb.ObjectId;
 const { getDb } = require("../util/database.s");
 
 class User {
-  constructor(username, email) {
+  constructor(username, email, cart, id) {
     this.username = username;
     this.email = email;
+    this.cart = cart; // {items: []}
+    this._id = id;
   }
 
   save() {
     const db = getDb();
     return db.collection("users").insertOne(this);
+  }
+
+  addToCart(product) {
+    // not perfect logic. just reference.
+    const db = getDb();
+    const updatedCart = { items: [{ ...product, quantity: 1 }] };
+    db.collection("users").updateOne(
+      { _id: new ObjectId(this._id) },
+      { $set: { cart: updatedCart } }
+    );
   }
 
   static findById(userId) {
