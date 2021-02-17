02d961374b3e7f649356bc9bbf01094cf3120566
kimjinsoo <roorooro9933@gmail.com>
2021-02-17 15:38:28 +0900

    set cookies and using it in express

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,11 +1,14 @@
 exports.getLogin = (req, res, next) => {
+  console.log(req.get("Cookie").split(";")[0].split("=")[1]);
   res.render("auth/login", {
     path: "/login",
     pageTitle: "Login",
+    isAuthenticated: req.isLoggedIn,
   });
 };
 
 exports.postLogin = (req, res, next) => {
+  res.setHeader("Set-Cookie", "loggedIn=true; Max-Age=10");
   res.redirect("/");
   // res.render("auth/login", {
   //   path: "/login",
