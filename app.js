b0d941d2b6a890c1170624a8be58b23e0c5b3eab
kimjinsoo <roorooro9933@gmail.com>
2021-02-17 15:58:03 +0900

    store session information in mongodb sessions collection

--- app.js
+++ app.js
@@ -4,11 +4,18 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const connectMongoose = require("./util/mongoose.s");
 const session = require("express-session");
+const MongoDBStore = require("connect-mongodb-session")(session);
+const MONGODB_URI = require("./util/mongodbUri.s");
 
 const errorController = require("./controllers/error");
 const User = require("./models/user");
 
 const app = express();
+
+const store = new MongoDBStore({
+  uri: MONGODB_URI,
+  collection: "sessions",
+});
 
 app.set("view engine", "ejs");
 app.set("views", "views");
@@ -19,8 +26,14 @@ const authRoutes = require("./routes/auth");
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
+
 app.use(
-  session({ secret: "my secret", resave: false, saveUninitialized: false })
+  session({
+    secret: "my secret",
+    resave: false,
+    saveUninitialized: false,
+    store: store,
+  })
 );
 
 app.use((req, res, next) => {
