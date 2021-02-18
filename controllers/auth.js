61e30cc44495320c8d3f3b39d02e49032dddcf86
kimjinsoo <roorooro9933@gmail.com>
2021-02-18 16:20:16 +0900

    add signup logic

--- controllers/auth.js
+++ controllers/auth.js
@@ -34,3 +34,36 @@ exports.postLogout = (req, res, next) => {
     res.redirect("/");
   });
 };
+
+exports.getSignup = (req, res, next) => {
+  res.render("auth/signup", {
+    path: "/signup",
+    pageTitle: "Signup",
+    isAuthenticated: req.isLoggedIn,
+  });
+};
+
+exports.postSignup = (req, res, next) => {
+  const email = req.body.email;
+  const password = req.body.password;
+  const confirmPassword = req.body.confirmPassword;
+
+  User.findOne({ email: email })
+    .then((userDoc) => {
+      if (userDoc) {
+        return res.redirect("/signup");
+      }
+      const user = new User({
+        email: email,
+        password: password,
+        cart: { items: [] },
+      });
+      return user.save();
+    })
+    .then((result) => {
+      res.redirect("/login");
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+};
