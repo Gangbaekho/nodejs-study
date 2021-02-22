77c0770a69c6b1b2a32e9fc13cbf0e128724b3a6
kimjinsoo <roorooro9933@gmail.com>
2021-02-22 13:34:10 +0900

    understanding application pdf content type and content disposition

--- controllers/shop.js
+++ controllers/shop.js
@@ -159,6 +159,11 @@ exports.getInvoice = (req, res, next) => {
     if (err) {
       return next(err);
     }
+    res.setHeader("Content-Type", "application/pdf");
+    res.setHeader(
+      "Content-Disposition",
+      'attachment; filename="' + invoiceName + '"'
+    );
     res.send(data);
   });
 };
